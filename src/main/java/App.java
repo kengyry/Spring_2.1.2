40213ae6d2ecb6fc31e40cdc4854253776dd8c9e
kengy <kengyry.ken@yandex.ru>
2020-09-26 17:27:01 +0500

    final commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean2 = (Cat) applicationContext.getBean("cat");
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Результат сравнения одинаковых бинов helloworld: " + (bean == bean1));
+        System.out.println("Результат сравнения одинаковых бинов cat: " + (bean2 == bean3));
     }
 }
